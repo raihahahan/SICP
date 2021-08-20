89c6eac44fce1d069e048a122b2fc12439600370
Muhd Raihan <muhammad.raihan.rizqullah.2019@vjc.sg>
2021-08-20 15:19:11 +0800

    Exercise 2.51

---
+++ Exercises/Chapter 2/2.2  Hierarchical Data and the Closure Property/2.2.4 A Picture Language (2.44 - 2.52)/Exercise 2.51.rkt
@@ -0,0 +1,106 @@
+#lang sicp
+(#%require sicp-pict)
+(define wave einstein)
+;; Exercise 2.51
+;; Vector implementation
+; 1st layer: constructors and selectors make-vect, xcor-vect, ycor-vect
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect vector)
+  (car vector))
+
+(define (ycor-vect vector)
+  (cdr vector))
+
+; 2nd layer: procedures add-vect, sub-vect, scale-vect
+
+(define (add-vect v1 v2)
+  (let ((x1 (xcor-vect v1))
+        (x2 (xcor-vect v2))
+        (y1 (ycor-vect v1))
+        (y2 (ycor-vect v2)))
+    (make-vect (+ x1 x2)
+               (+ y1 y2))))
+
+(define (sub-vect v1 v2)
+  (let ((x1 (xcor-vect v1))
+        (x2 (xcor-vect v2))
+        (y1 (ycor-vect v1))
+        (y2 (ycor-vect v2)))
+    (make-vect (- x1 x2)
+               (- y1 y2))))
+
+(define (scale-vect s vect)
+  (let ((x (xcor-vect vect))
+        (y (ycor-vect vect)))
+    (make-vect (* s x)
+               (* s y))))
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+(define (rotate-cc-90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+
+(define (rotate-cc-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+
+;; First implementation: analogous to beside
+(define (below painter_b painter_t)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter painter_b
+                              (make-vector 0.0 0.0)
+                              (make-vector 1.0 0.0)
+                              split-point))
+          (paint-top
+           (transform-painter painter_t
+                              split-point
+                              (make-vector 1.0 0.5)
+                              (make-vector 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+;; Second implementation: use beside
+(define (below-1 painter_b painter_t)
+  (rotate-cc-90 (beside (rotate-cc-270 painter_b)
+                        (rotate-cc-270 painter_t))))
+                
+
+      
+      
+  
+  
---
+++ Exercises/Chapter 2/2.2  Hierarchical Data and the Closure Property/2.2.4 A Picture Language (2.44 - 2.52)/Exercise 2.52.rkt
@@ -0,0 +1,5 @@
+#lang sicp
+(#%require sicp-pict)
+(define wave einstein)
+
+;; Exercise 2.52
