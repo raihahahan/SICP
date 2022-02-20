5728e42fead8cc7bc4096e22ed08dcf4b78b9717
Muhd Raihan <muhammad.raihan.rizqullah.2019@vjc.sg>
2022-02-20 18:27:48 +0800

    Fixed exercise 3.7

--- Exercises/Chapter 3/3.1 Assignment and Local State/3.1.1 Local State Variables/Exercise 3.7.rkt
+++ Exercises/Chapter 3/3.1 Assignment and Local State/3.1.1 Local State Variables/Exercise 3.7.rkt
@@ -1,15 +1,24 @@
 #lang racket
-;; EXPLANATION OF SOLUTION
-
-; There are 2 main local variables with state: balance and password
-; Balance is shared amongst the accounts in joint-accounts (or not shared if it is not joint).
-; Password is unique to each account.
-; We create an account with (create-account) and give our balance and password.
-; Withdrawing and depositing works the same way as 3.3, where the procedure checks the password and do the necessary operations.
-; To add joint account, simply return the procedure itself (recursively), with a new password variable but same balance state.
 
 (define (make-joint acc1 acc1-pw acc2-pw)
-  ((acc1 acc1-pw 'add-joint) acc2-pw))
+  (define (withdraw amount)
+    ((acc1 acc1-pw 'withdraw) amount))
+
+  (define (deposit amount)
+    ((acc1 acc1-pw 'deposit) amount))
+
+   (define (dispatch pw m)
+    (cond
+      ((or (not (eq? acc2-pw pw))
+           (not (acc1 acc1-pw 'auth)))
+       (lambda (a) "Incorrect password"))
+      ((eq? m 'withdraw) withdraw)
+      ((eq? m 'deposit) deposit)
+      (else
+         (error "Unknown request: MAKE-ACCOUNT"
+                m))))
+    dispatch)
+ 
 
 (define (make-account balance password)
   
@@ -33,17 +42,22 @@
       ((eq? m 'withdraw) withdraw)
       ((eq? m 'deposit) deposit)
       ((not (eq? pw password)) (lambda (a) "Incorrect password"))
-      ((eq? m 'add-joint) (lambda (new-pw) (make-account balance new-pw)))
+      ((eq? m 'auth) (eq? pw password))
       (else
          (error "Unknown request: MAKE-ACCOUNT"
                 m))))
     dispatch)
-          
+
 (define paul (make-account 100 'paul))
-((paul 'paul 'withdraw) 10)
-((paul 'pasul 'withdraw) 10)
+((paul 'paul 'withdraw) 10) ; 90
+((paul 'pasul 'withdraw) 10) ; "Incorrect password"
+(define pete-wrong
+  (make-joint paul 'pausl 'new-pete)) 
 (define pete
-  (make-joint paul 'paul 'new-pete))
-((pete 'new-pete 'withdraw) 10)
+  (make-joint paul 'paul 'new-pete)) 
+((pete 'new-pete 'withdraw) 10)  ; 80
+((paul 'paul 'deposit) 200) ; 280
+((pete 'new-pete 'deposit) 0) ; 280
+((paul 'paul 'deposit) 0) ; 280
         
    
