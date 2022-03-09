95fbe4b0dc188da7eb940aff1089c7060c63340e
Muhd Raihan <muhammad.raihan.rizqullah.2019@vjc.sg>
2022-03-09 19:43:55 +0800

    new exercises

---
+++ Exercises/Chapter 3/3.1 Assignment and Local State/3.1.1 Local State Variables/Exercise 3.10.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (make-withdraw initial-amount)
+  ((lambda (balance)
+     (lambda (amount)
+       (if (>= balance amount)
+           (begin (set! balance (- balance amount))
+                  balance)
+           "Insufficient balance"))
+     initial-amount)))
+       
